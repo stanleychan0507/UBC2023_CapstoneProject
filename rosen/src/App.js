3a28ce3cbd02d122aa215606e952fc4438ae04ca
Hongaponga <liewshanhong@outlook.com>
2022-11-03 23:23:07 -0700

    flask setup

--- rosen/src/App.js
+++ rosen/src/App.js
@@ -1,8 +1,13 @@
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() =>{
+    fetch("http://localhost:5000").then(r => console.log(r.json())).then(console.log)
+  })
     return(
       <>
         <Navbar/>
