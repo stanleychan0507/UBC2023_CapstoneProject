0a2761f2861849d8f52a84931b2b48d1f949c881
Pak In Chan (Stanley) <78773070+stanleychan0507@users.noreply.github.com>
2022-11-04 22:34:42 -0700

    Upload authentication set up

--- rosen/src/App.js
+++ rosen/src/App.js
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
-import Authentication from "./components/Authentication";
-import Preferences from "./components/Preferences";
+//import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//import Authentication from "./components/Authentication";
+//import Preferences from "./components/Preferences";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
