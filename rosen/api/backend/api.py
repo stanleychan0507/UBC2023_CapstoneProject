b34fed83310959bacca6c418babf621aeaf845ba
Hongaponga <liewshanhong@outlook.com>
2022-12-02 01:41:53 -0800

    Second commit
    removed errors, ref photos and video uploads to a folder

--- rosen/api/backend/api.py
+++ rosen/api/backend/api.py
@@ -6,8 +6,8 @@ from flask_cors import CORS
 api = Flask(__name__)
 cors = CORS(api)
 
-UPLOAD_PHOTOS = '../photos'
-UPLOAD_VIDEOS = '../videos'
+UPLOAD_PHOTOS = './photos'
+UPLOAD_VIDEOS = './videos'
 api.config['UPLOAD_PHOTOS'] = UPLOAD_PHOTOS
 api.config['UPLOAD_VIDEOS'] = UPLOAD_VIDEOS
 
@@ -19,11 +19,11 @@ def index():
 @api.route('/api/upload', methods=['GET','POST'])
 def upload():
     if request.method == 'POST':
-        photo = request.form.get['photo']
-        #video = request.form.get('video')
-        #photo.save(os.path.join(api.config['UPLOAD_PHOTOS'],'download.jpg'))
-        #video.save(os.path.join(api.config['UPLOAD_PHOTOS'],'download.jpg'))
-        return "server has received files"
+        photo = request.files['photo']
+        video = request.files['video']
+        photo.save(os.path.join(api.config['UPLOAD_PHOTOS'],photo.filename))
+        video.save(os.path.join(api.config['UPLOAD_VIDEOS'],video.filename))
+        return "Image has been successfully uploaded"
 
 if __name__ == "__main__":
     api.run(debug=True)
