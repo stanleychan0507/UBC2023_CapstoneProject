05a080c3dccfb8d9dc14b3cdb9c7275a8093400d
Lucadeverteuil <lucadeverteuil@gmail.com>
2022-12-06 13:31:20 -0800

    Added Model Help
    Still needs some work however the skeleton is ready to use

--- rosen/src/components/Navbar.js
+++ rosen/src/components/Navbar.js
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 import RG from "../images/RosenLogo.webp";
 import { Link } from "react-router-dom";
 import "../CSS/Navbar.css";
+import Modal from 'react-bootstrap/Modal';
 
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const handleShow = () =>setShow(true);
+  const handleClose = () => setShow(false);
+
   return (
     <div>
       <nav className="navbar">
         <img src={RG} className="logo" alt="Rosen Group logo" />
         <ul>
           <li>
+            <button className="help" onClick={handleShow}> ? </button>
               <Link to="login">Login</Link>
           </li>
         </ul>
       </nav>
+      <Modal show= {show} onHide = {handleClose} centered>
+        <Modal.Header> 
+          <Modal.Title>
+            How To Run The Program
+          </Modal.Title> 
+        </Modal.Header>
+       <Modal.Body> 
+          <div> 
+          Step1: 
+          
+          </div>
+          <div> 
+          Step2: 
+          
+          </div>
+          <div> 
+          Step3: 
+          
+          </div>
+          <div> 
+           Step4: 
+          
+         </div>
+         <div> 
+            Volia
+          
+        </div>
+       </Modal.Body>
+        
+        
+         </Modal>
+
+
+
     </div>
   );
 }
