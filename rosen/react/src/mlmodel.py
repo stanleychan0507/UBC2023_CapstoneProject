404cb2df46d06cc1603f0ff79b2d8f124e4d4654
Pak In Chan (Stanley) <78773070+stanleychan0507@users.noreply.github.com>
2023-01-27 20:29:32 -0800

    Testing
    Testing

--- rosen/react/src/mlmodel.py
+++ rosen/react/src/mlmodel.py
@@ -19,6 +19,17 @@ regressor.fit(x, y)
 # Pickle serializes objects so they can be saved to a file, and loaded in a program again later on.
 pickle.dump(regressor, open('model.pkl','wb'))
 
+# Importing the proper classes
+# from DeepImageSearch import Index,LoadData,SearchImage
+# load the Images from the Folder (You can also import data from multiple folder in python list type)
+# image_list = LoadData().from_folder(['images','wiki-images'])
+# For Faster Serching we need to index Data first, After Indexing all the meta data stored on the local path
+# Index(image_list).Start()
+# for searching you need to give the image path and the number of similar image you want
+# SearchImage().get_similar_images(image_path=image_list[0],number_of_images=5)
+# If you want to plot similar images the you can use this method, It will plot 16 most similar images from the data index
+# SearchImage().plot_similar_images(image_path = image_list[0])
+
 '''
 #Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
