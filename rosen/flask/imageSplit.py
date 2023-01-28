3ecb5af98426fb82c2e0dca2ba336bef005793d9
Lucadeverteuil <lucadeverteuil@gmail.com>
2023-01-27 17:57:05 -0800

    added comments to code
    i added comments to split image

--- rosen/flask/imageSplit.py
+++ rosen/flask/imageSplit.py
@@ -1,6 +1,8 @@
 import cv2 
 import os
 #this will cut the photos
+
+#this method first gets video from filepath in the while loop it counts number of frams cut. note we only have 10 frame limit as a test
 def split(filename):
     cap = cv2.VideoCapture(filename)
     i=1
