35635ba8b4f80ba9dfd733d4d919a48cd519c7bd
Luca deVerteuil <lucadeverteuil@gmail.com>
2023-03-30 22:04:40 -0700

    Frames time calucated and added as well as fps made better
    I added some chnages to our time stamp and fps to make it a bit more acuurate

--- rosen/flask/imageSplit.py
+++ rosen/flask/imageSplit.py
@@ -14,33 +14,48 @@ With purpose of this Function is to split a video into frames and save them as i
 #this will cut the photos
 
 #this method first gets video from filepath in the while loop it counts number of frams cut. note we only have 10 frame limit as a test
-def split(filepath, filename, frames):
-    cap = cv2.VideoCapture(filepath)
-    # Get the total number of frames
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    # Get the frame rate
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    # Calculate the video length in seconds
-    length_seconds = frame_count / fps
-    #delcare counter to know when to stop cutting photoes and for image names
-    array = []
-    #delcare counter to know when to stop cutting photoes and for image names
-    count = 0
-    #declaire where we will save the images
-    path2 = "./videos/"+filename+"/Photos"
-    #while loop cuts the image every 50 frames
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if count == 30:
-            break;
-        if ret == False:
-            break
-    #if stament for every 50 frames
-        if count < frames*length_seconds and count < 30:
-            #saves to folder images
-            cv2.imwrite(os.path.join(path2,"filename"+str(count)+'.jpg'),frame)
-            print("Saved")
-            count +=1
-            array.append(datetime.datetime.fromtimestamp(cap.get(cv2.CAP_PROP_POS_MSEC)/1000.0).strftime('%H:%M:%S'))
+import datetime
+import cv2 
+import os
 
-    return array
+'''
+This uses the API from openCV
+With purpose of this Function is to split a video into frames and save them as images for the user to use later 
+1. first we call the folder where the image is saved
+2. use a while loop to to cut frames into 50 frames per secound 
+3. save images into file  
+'''
+
+
+#this will cut the photos
+
+#this method first gets video from filepath in the while loop it counts number of frams cut. note we only have 10 frame limit as a test
+def split(filepath, filename, frames):
+    vidcap = cv2.VideoCapture(filepath)
+
+    # Initialize a variable to keep track of the current frame number
+    frame_number = 0
+    count = 0
+    # Loop over the frames of the video
+    while True:
+        # Read the next frame from the video
+        success, image = vidcap.read()
+        path2 = "./videos/"+filename+"/Photos"
+        # If reading the frame was unsuccessful, break out of the loop
+        if not success:
+            break
+        elif count == 180:
+            break
+        
+       
+        if frame_number % frames == 0:
+             cv2.imwrite(os.path.join(path2,"filename"+str(frame_number)+'.jpg'),image)
+             count +=1
+        
+        # Increment the frame number
+        frame_number += 1
+        
+    # Release the video file
+    vidcap.release() 
+
+    return "Completed"
